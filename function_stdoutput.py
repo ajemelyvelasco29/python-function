ff67b489b01c1f3a5d3a480b0623d8b1150d1683
April Jemely Velasco <apriljemelyvelasco@yahoo.com>
2016-03-09 16:45:17 +0800

    changed function name from MyFunc to FuncOut

--- function_stdoutput.py
+++ function_stdoutput.py
@@ -3,9 +3,9 @@
 #Date: March 7, 2016
 #Purpose: Python Script for displaying ouput in three ways
 
-def FuncOut(name, age):
+def MyFunc(name, age):
           print "Hi! My name is ", name + "and my age is", age
           print "Hi! My name is %s and my age is %d" %(name, age)
           print "Hi! My name is {} and my age is {}".format(name,age)
 
-print FuncOut("April Jemely Velasco", 19)
+print MyFunc("April Jemely Velasco", 19)
